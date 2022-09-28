ce97eb26032582f9ea3c4e06c4a5325d6f0a4ebe
rucarpe <rucarpe@icloud.com>
2022-09-28 20:38:31 +0200

    Actualización

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rucarpe Games</title>
+</head>
+<body>
+    <!--HEADER - CABECERA-->
+    <header>
+        <a  href="index.php" id="logo" class="page-title"> <h1>rucarpeGames.io</h1></a>
+    </header>
+    <!--MENU-->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <ul id="main-menu">
+            <li id="menu-item-1">Home</li>
+            <li id="menu-item-2">Categoría 1</li>
+            <li id="menu-item-3">Categoría 2</li>
+            <li id="menu-item-4">Categoría 3</li>
+            <li id="menu-item-5">Categoría 4</li>
+            <li id="menu-item-6">Sobre nosotros</li>
+            <li id="menu-item-7">Contacto</li>
+        </ul>
+    </nav>
+    <!--ASAIDS -->
+    <!--MAIN CONTENT-->
+    <!--FOOTER-->    
+</body>
+</html>
